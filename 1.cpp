c28738e946228944252c8c29e40a23a72ed01361
ahmed <azakaria76@gmail.com>
2022-03-18 02:40:57 +0200

    editied

--- 1.cpp
+++ 1.cpp
@@ -14,17 +14,15 @@ cin >> first_name;
 cout << "Dear " << first_name << ",\n" << "How are you? I am fine. I miss you.\n";
 cout << "Enter another friend\n";
 cin >> friend_name;
-cout << "Have you seen " << friend_name << " lately?\n enter sex\n";
+cout << "Have you seen " << friend_name << " lately?\nEnter sex\n";
 cin >> friend_sex;
 if (friend_sex == 'm')  
 cout << "If you see " << friend_name << " please ask him to call me\n";
 if (friend_sex == 'f')  
-cout << "If you see " << friend_name << " please ask her to call me\n Enter Age";
+cout << "If you see " << friend_name << " please ask her to call me\nEnter Age ";
 cin >> age;
-cout << "I hear you just had a birthday and you are " << age << "years old";
-
-
-cout << "Yours Sincerely\n\nAhmed";
+cout << "I hear you just had a birthday and you are " << age << " years old\n";
+cout << "\nYours Sincerely\nAhmed";
 
 
 }
