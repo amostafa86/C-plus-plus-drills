9a5f887af96f804b55c21884d9d5927bff9bb343
ahmed <azakaria76@gmail.com>
2022-03-18 02:28:33 +0200

    edited

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+cout << "Enter the name of the person you want to write to\n";
+string first_name;
+string friend_name;
+char friend_sex;
+int age;
+cin >> first_name;
+cout << "Dear " << first_name << ",\n" << "How are you? I am fine. I miss you.\n";
+cout << "Enter another friend\n";
+cin >> friend_name;
+cout << "Have you seen " << friend_name << " lately?\n enter sex\n";
+cin >> friend_sex;
+if (friend_sex == 'm')  
+cout << "If you see " << friend_name << " please ask him to call me\n";
+if (friend_sex == 'f')  
+cout << "If you see " << friend_name << " please ask her to call me\n Enter Age";
+cin >> age;
+cout << "I hear you just had a birthday and you are " << age << "years old";
+
+
+cout << "Yours Sincerely\n\nAhmed";
+
+
+}
